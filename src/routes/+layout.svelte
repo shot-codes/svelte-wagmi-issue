01975db8d5037a42fb7a178a11c4dfff021265e5
Nicolai Verbaarschot <nicolai.verbaarschot@gmail.com>
2024-06-14 15:58:46 +0200

    Initial commit.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { defaultConfig, wagmiLoaded } from "svelte-wagmi";
+  import { onMount } from "svelte";
+  import {
+    PUBLIC_ALCHEMY_KEY,
+    PUBLIC_WALLETCONNECT_PROJECTID,
+  } from "$env/static/public";
+
+  onMount(async () => {
+    const erckit = defaultConfig({
+      appName: "erc.kit",
+      walletConnectProjectId: PUBLIC_WALLETCONNECT_PROJECTID,
+      alchemyId: PUBLIC_ALCHEMY_KEY,
+    });
+
+    await erckit.init();
+  });
+</script>
+
+{#if $wagmiLoaded}
+  <slot />
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import {
+    WC,
+    connected,
+    signerAddress,
+    disconnectWagmi,
+  } from "svelte-wagmi";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+{#if $connected}
+  <h1>{$signerAddress}</h1>
+  <button on:click={disconnectWagmi}>disconnect</button>
+{:else}
+  <button on:click={WC}>walletconnect connect</button>
+{/if}
